772f81804c1102b3a7cacbea88c11a3b83784cdb
TheMlok <martin.p@mixit.cz>
2024-02-19 08:39:02 +0100

    adds constants

--- src/calculations/dew_point.rs
+++ src/calculations/dew_point.rs
@@ -3,7 +3,6 @@ use std::f64;
 
 // TODO: add attribute for degree kind
 //       add ability to count in fahrenheits, use crate function to recalculate the temperature
-//       add constants somewhere
 //       add ablility to set given atm. pressure
 
 /// Calculates dew point using Magnus-Tetens formula
@@ -15,15 +14,13 @@ use std::f64;
 /// let humidity = 62.4;
 /// let result = 15.0;
 ///
-/// let dew_point = meteo_tools::dew_point(&temperature, &humidity);
+/// let dew_point = meteo_tools::celsius_dew_point(&temperature, &humidity);
 ///
 /// assert_eq!(dew_point, result);
 /// ```
-pub fn dew_point(temperature: &f64, humidity: &f64) -> f64 {
+pub fn celsius_dew_point(temperature: &f64, humidity: &f64) -> f64 {
     let gamma = ((LATENT_HEAT_OF_VAPORIZATION * temperature)
         / (STANDARD_CONDENSATION_POINT + temperature))
         + (humidity / 100.0).ln();
-    let dew_point =
-        (STANDARD_CONDENSATION_POINT * gamma / (LATENT_HEAT_OF_VAPORIZATION - gamma)).round();
-    dew_point
+    (STANDARD_CONDENSATION_POINT * gamma / (LATENT_HEAT_OF_VAPORIZATION - gamma)).round()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,4 @@ mod calculations;
 mod constants;
 mod conversions;
 
-pub use self::calculations::dew_point::dew_point;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use self::calculations::dew_point::celsius_dew_point;
