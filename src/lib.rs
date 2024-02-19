7a1333ea683af2f077fe11791c0c356ea4c046b7
TheMlok <martin.p@mixit.cz>
2024-02-19 13:56:56 +0100

    todo

--- src/lib.rs
+++ src/lib.rs
@@ -26,10 +26,10 @@ pub use self::conversions::temperature::fahrenheit_to_kelvin;
 pub use self::conversions::temperature::kelvin_to_celsius;
 pub use self::conversions::temperature::kelvin_to_fahrenheit;
 
-// TODO: conversion to kelvin from c and f and vice versa
-// TODO: mixing ratio, absolute relative_humidity, Specific relative_humidity, Mixing Ratio, Absolute relative_humidity, Vapor pressure, Relative saturation
+// TODO: mixing ratio, Specific relative_humidity, Mixing Ratio, Absolute relative_humidity, Vapor pressure, Relative saturation
 // TODO: heat index variants: Steadman's Apparent Temperature, Australian Apparent Temperature, New Zealand Apparent Temperature
 
-pub fn meteo_round(number: &f64) -> f64 {
+/// Rounds given value to 4 decimal places
+fn meteo_round(number: &f64) -> f64 {
     (number * 10000.0).round() / 10000.0
 }
